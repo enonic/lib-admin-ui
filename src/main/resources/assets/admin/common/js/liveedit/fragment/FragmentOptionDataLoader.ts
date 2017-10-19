64c9412be07b3a12a9a4e72cae36cbc8075c9b73
vbradnitski <vbr@enonic.com>
2017-10-19 14:29:02 +0300

    Errors in fragment selector in the PageEditor #115

--- src/main/resources/assets/admin/common/js/liveedit/fragment/FragmentOptionDataLoader.ts
+++ src/main/resources/assets/admin/common/js/liveedit/fragment/FragmentOptionDataLoader.ts
@@ -38,6 +38,14 @@ module api.liveedit.fragment {
             return new FragmentOptionDataRequest();
         }
 
+        isPartiallyLoaded(): boolean {
+            return this.request.isPartiallyLoaded();
+        }
+
+        resetParams() {
+            this.request.resetParams();
+        }
+
         setParentSitePath(parentSitePath: string): FragmentOptionDataLoader {
             this.request.setParentSitePath(parentSitePath);
             this.request.setQueryExpr();
