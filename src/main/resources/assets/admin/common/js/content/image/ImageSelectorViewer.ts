765fe550df1abdd879863780abf1ce4b928f7f73
Pavel Milkevich <pavel.milkevich@gmail.com>
2017-11-08 17:22:40 +0300

    Show image display name and path in gallery mode #144

--- src/main/resources/assets/admin/common/js/content/image/ImageSelectorViewer.ts
+++ src/main/resources/assets/admin/common/js/content/image/ImageSelectorViewer.ts
@@ -22,5 +22,9 @@ module api.content.image {
         resolveIconUrl(object: ImageTreeSelectorItem): string {
             return object.getImageUrl() + '&size=270';
         }
+
+        resolveHint(object: api.content.image.ImageTreeSelectorItem): string {
+            return object.getPath().toString();
+        }
     }
 }
