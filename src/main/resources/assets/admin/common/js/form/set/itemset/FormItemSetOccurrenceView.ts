84d385fc7d908592246da2e5db8ffe95135726ad
Alan Semenov <ase@enonic.com>
2017-12-07 14:39:35 +0100

    Set title of item-set on blur of its inputs (#211)

--- src/main/resources/assets/admin/common/js/form/set/itemset/FormItemSetOccurrenceView.ts
+++ src/main/resources/assets/admin/common/js/form/set/itemset/FormItemSetOccurrenceView.ts
@@ -29,24 +29,25 @@ module api.form {
             this.formItemLayer = new FormItemLayer(config.context);
         }
 
-        public layout(validate: boolean = true): wemQ.Promise<void> {
-            return super.layout(validate).then(() => {
+        private setTitle() {
+            const firstNonEmptyInput = wemjq(this.formSetOccurrencesContainer.getHTMLElement())
+                .find('.input-wrapper input, .input-wrapper textarea').toArray()
+                .find(input => {
+                    const value = (input.nodeName === 'INPUT') ? input.value : api.util.StringHelper.htmlToString(input.value);
+                    return value.trim().length > 0;
+                });
 
-                const firstNonEmptyInput = wemjq(this.formSetOccurrencesContainer.getHTMLElement())
-                    .find('.input-wrapper input, .input-wrapper textarea').toArray()
-                    .find(input => {
-                        const value = (input.nodeName === 'INPUT') ? input.value : api.util.StringHelper.htmlToString(input.value);
-                        return value.trim().length > 0;
-                    });
-
-                if (firstNonEmptyInput) {
-                    if (firstNonEmptyInput.nodeName === 'INPUT') {
-                        this.label.setTitle(firstNonEmptyInput.value);
-                    } else {
-                        this.label.setTitle(api.util.StringHelper.htmlToString(firstNonEmptyInput.value)); // Strip HTML tags
-                    }
+            if (firstNonEmptyInput) {
+                if (firstNonEmptyInput.nodeName === 'INPUT') {
+                    this.label.setTitle(firstNonEmptyInput.value);
+                } else {
+                    this.label.setTitle(api.util.StringHelper.htmlToString(firstNonEmptyInput.value)); // Strip HTML tags
                 }
-            });
+            }
+        }
+
+        public layout(validate: boolean = true): wemQ.Promise<void> {
+            return super.layout(validate).then(() => this.setTitle());
         }
 
         protected subscribeOnItemEvents() {
@@ -74,6 +75,8 @@ module api.form {
                     const summaryAndStatus = api.content.ContentSummaryAndCompareStatus.fromContentSummary(content);
                     new api.content.event.EditContentEvent([summaryAndStatus]).fire();
                 });
+
+                formItemView.onBlur(() => this.setTitle());
             });
         }
 
