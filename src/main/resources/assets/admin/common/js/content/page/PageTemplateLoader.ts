3b07340536a62340492bc6ff83be678c93809bdd
Pavel Milkevich <pavel.milkevich@gmail.com>
2017-12-21 17:29:16 +0300

    Template selector - List templates in the same order as in the grid #402

--- src/main/resources/assets/admin/common/js/content/page/PageTemplateLoader.ts
+++ src/main/resources/assets/admin/common/js/content/page/PageTemplateLoader.ts
@@ -3,12 +3,11 @@ module api.content.page {
     import ContentJson = api.content.json.ContentJson;
     import ListContentResult = api.content.resource.result.ListContentResult;
 
-    export class PageTemplateLoader extends api.util.loader.BaseLoader<ListContentResult<ContentJson>, PageTemplate> {
+    export class PageTemplateLoader
+        extends api.util.loader.BaseLoader<ListContentResult<ContentJson>, PageTemplate> {
 
         constructor(request: PageTemplateResourceRequest<ListContentResult<ContentJson>, PageTemplate[]>) {
             super(request);
-
-            this.setComparator(new api.content.page.PageTemplateByDisplayNameComparator());
         }
 
         filterFn(template: PageTemplate) {
