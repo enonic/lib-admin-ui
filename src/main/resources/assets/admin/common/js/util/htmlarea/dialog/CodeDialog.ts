60ee6ad77be1b53b95434ad5a2f66d61f823a39e
Alan Semenov <ase@enonic.com>
2018-01-02 16:35:27 +0100

    Fixed layout in the HTML Area's Source code dialog (#282)
    (cherry picked from commit 1294c73)

--- src/main/resources/assets/admin/common/js/util/htmlarea/dialog/CodeDialog.ts
+++ src/main/resources/assets/admin/common/js/util/htmlarea/dialog/CodeDialog.ts
@@ -32,15 +32,7 @@ module api.util.htmlarea.dialog {
 
             this.textArea.setValue(this.getEditor().getContent({source_view: true}));
             this.getEl().setAttribute('spellcheck', 'false');
-            this.resetHeight();
             this.textArea.giveFocus();
-        }
-
-        private resetHeight() {
-            // textarea has 'height' style property updated on focus, shown events etc, overriding it gently
-            const height: number = this.getEditor().getParam('code_dialog_height', Math.min(tinymce.DOM.getViewPort().h - 200, 500));
-            this.textArea.getEl().setMinHeightPx(height);
-            this.textArea.getEl().setMaxHeightPx(height);
         }
 
         protected initializeActions() {
