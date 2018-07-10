53b14a681b1ce9b525a063d8d38b4ef0fa34be10
Alex Shklianko <asl@enonic.com>
2018-07-10 12:11:01 +0300

    CKE: don't destroy editor if it's iframe was removed #693
    -Not invoking editor's destroy when occurence removed
    -Stopped triggereing selection change on blur as it seems to be not needed anymore

--- src/main/resources/assets/admin/common/js/util/htmlarea/editor/HTMLAreaBuilderCKE.ts
+++ src/main/resources/assets/admin/common/js/util/htmlarea/editor/HTMLAreaBuilderCKE.ts
@@ -354,7 +354,6 @@ module api.util.htmlarea.editor {
             });
 
             ckeditor.on('blur', (e: eventInfo) => {
-                e.editor.getSelection().reset(); // that makes cke cleanup
 
                 if (this.hasActiveDialog) {
                     e.stop();
