716700ffe496520b58bb7657c5e3919b6502c265
Alex Shklianko <asl@enonic.com>
2018-07-10 17:10:31 +0300

    Insert link dialog - Validate URL #679

--- src/main/resources/assets/admin/common/js/ui/form/Validators.ts
+++ src/main/resources/assets/admin/common/js/ui/form/Validators.ts
@@ -13,6 +13,13 @@ module api.ui.form {
             let value = input.getValue();
             return !regexEmail.test(value) ? i18n('field.value.invalid') : undefined;
         }
+
+        public static validUrl(input: api.dom.FormInputEl): string {
+            let regexUrl =
+                /^((http(s)?:\/\/.)?(www\.)?[a-zA-Z0-9][-a-zA-Z0-9@:%._\+~#=]{1,255})\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+            let value = input.getValue();
+            return !regexUrl.test(value) ? i18n('field.value.invalid') : undefined;
+        }
     }
 
 }
