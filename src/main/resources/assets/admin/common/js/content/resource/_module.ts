592e19d31f331540c690484a6e9994c01cadb357
Mikita Taukachou <edloidas@gmail.com>
2018-07-23 14:45:06 +0300

    Move classes from api.content #695
    Moved `Permissions` requests.

--- src/main/resources/assets/admin/common/js/content/resource/_module.ts
+++ src/main/resources/assets/admin/common/js/content/resource/_module.ts
@@ -3,14 +3,10 @@
 ///<reference path='ContentSummaryPreLoader.ts' />
 ///<reference path='ContentSummaryLoader.ts' />
 ///<reference path='ContentResourceRequest.ts' />
-///<reference path='ApplyContentPermissionsRequest.ts' />
 ///<reference path='GetContentByIdRequest.ts' />
 ///<reference path='GetContentSummaryByIds.ts' />
 ///<reference path='GetContentByPathRequest.ts' />
-///<reference path='GetContentPermissionsByIdRequest.ts' />
-///<reference path='GetContentPermissionsByIdsRequest.ts' />
 ///<reference path='GetPermittedActionsRequest.ts' />
-///<reference path='GetContentRootPermissionsRequest.ts' />
 ///<reference path='GetContentAttachmentsRequest.ts' />
 ///<reference path='ListContentByIdRequest.ts' />
 ///<reference path='isContentReadOnlyRequest.ts' />
@@ -26,7 +22,6 @@
 ///<reference path='WidgetDescriptorResourceRequest.ts' />
 ///<reference path='GetWidgetsByInterfaceRequest.ts' />
 ///<reference path='GetDescendantsOfContentsRequest.ts' />
-///<reference path='GetEffectivePermissionsRequest.ts' />
 ///<reference path='GetContentIdsByParentRequest.ts' />
 ///<reference path='HasUnpublishedChildrenRequest.ts' />
 ///<reference path='ContentTreeSelectorItem.ts' />
