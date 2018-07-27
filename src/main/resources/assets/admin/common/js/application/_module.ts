8568c6c47049f735d8806aea7aeaad32f983a1fd
Mikita Taukachou <edloidas@gmail.com>
2018-07-27 11:31:26 +0300

    Move classes from api.application #713
    Moved market classes.

--- src/main/resources/assets/admin/common/js/application/_module.ts
+++ src/main/resources/assets/admin/common/js/application/_module.ts
@@ -1,6 +1,4 @@
-///<reference path='ResourcePath.ts' />
 ///<reference path='ApplicationKey.ts' />
-///<reference path='ApplicationResourceKey.ts' />
 ///<reference path='Application.ts' />
 ///<reference path='ApplicationResourceRequest.ts' />
 ///<reference path='ApplicationInstallResult.ts' />
@@ -21,11 +19,6 @@
 ///<reference path='ApplicationUploaderEl.ts' />
 ///<reference path='ApplicationUploadStartedEvent.ts' />
 ///<reference path='InstallUrlApplicationRequest.ts' />
-///<reference path='MarketApplication.ts' />
-///<reference path='MarketApplicationFetcher.ts' />
-///<reference path='MarketApplicationResponse.ts' />
-///<reference path='MarketApplicationMetadata.ts' />
-///<reference path='ListMarketApplicationsRequest.ts' />
 ///<reference path='UninstallApplicationRequest.ts' />
 ///<reference path='ListSiteApplicationsRequest.ts' />
 ///<reference path='ListAuthApplicationsRequest.ts' />
--- src/main/resources/assets/admin/common/js/application/json/_module.ts
+++ src/main/resources/assets/admin/common/js/application/json/_module.ts
@@ -1,4 +1,2 @@
 ///<reference path='ApplicationJson.ts' />
-///<reference path='MarketApplicationJson.ts' />
-///<reference path='MarketApplicationsListJson.ts' />
 ///<reference path='ApplicationInstallResultJson.ts' />
