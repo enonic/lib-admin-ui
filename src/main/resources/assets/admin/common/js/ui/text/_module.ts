708610bc37ce27555321e3d016e060ad359b7e0c
Mikita Taukachou <edloidas@gmail.com>
2018-07-31 19:36:23 +0300

    Move remaining classes and styles #730

--- src/main/resources/assets/admin/common/js/ui/text/_module.ts
+++ src/main/resources/assets/admin/common/js/ui/text/_module.ts
@@ -1,9 +1,6 @@
 ///<reference path='TextInput.ts' />
-///<reference path='InPlaceTextInput.ts' />
 ///<reference path='AutosizeTextInput.ts' />
 ///<reference path='PasswordInput.ts' />
 ///<reference path='TextArea.ts' />
-///<reference path='InPlaceTextArea.ts' />
-///<reference path='PasswordGenerator.ts' />
 ///<reference path='EmailInput.ts' />
 ///<reference path='FileInput.ts' />
