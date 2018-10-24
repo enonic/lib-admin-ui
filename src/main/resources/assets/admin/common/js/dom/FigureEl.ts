5428515606d45fa2b8d8f4106091545795258ab9
Alan Semenov <ase@enonic.com>
2018-10-24 20:53:28 +0200

    Implemented classes for figure and figcaption elements (#854)

---
+++ src/main/resources/assets/admin/common/js/dom/FigureEl.ts
@@ -0,0 +1,39 @@
+module api.dom {
+
+    export class FigureEl
+        extends Element {
+
+        private image: ImgEl;
+        private caption: FigcaptionEl;
+
+        constructor(image: api.dom.ImgEl, className?: string) {
+            super(new NewElementBuilder().setTagName('figure').setClassName(className));
+
+            this.appendChild(image);
+        }
+
+        setCaption(caption: string) {
+
+            if (this.caption) {
+                this.caption.setCaption(caption);
+            } else {
+                this.caption = new FigcaptionEl(caption);
+                this.insertAfterEl(this.image);
+            }
+        }
+    }
+
+    class FigcaptionEl
+        extends Element {
+
+        constructor(caption: string) {
+            super(new NewElementBuilder().setTagName('figcaption'));
+
+            this.setCaption(caption);
+        }
+
+        setCaption(caption: string) {
+            this.setHtml(caption);
+        }
+    }
+}
