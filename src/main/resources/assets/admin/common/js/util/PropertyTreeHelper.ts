18ef7cd6c0132f0ff5161adf92b94b5a2a583833
Alex Shklianko <asl@enonic.com>
2019-05-16 10:22:09 +0200

    ID Provider creation - Empty property set #999
    -Codacy upd
    
    (cherry picked from commit 796f034130efa27f22dd60b9ea43c2feb55b16d8)

--- src/main/resources/assets/admin/common/js/util/PropertyTreeHelper.ts
+++ src/main/resources/assets/admin/common/js/util/PropertyTreeHelper.ts
@@ -12,14 +12,20 @@ module api.util {
         }
 
         static propertyTreesEqual(config: PropertyTree, otherConfig: PropertyTree, ignoreEmptyValues: boolean = true): boolean {
-            if (ignoreEmptyValues && (!config || config.isEmpty()) && (!otherConfig || otherConfig.isEmpty())) {
+            if (ignoreEmptyValues) {
+                return PropertyTreeHelper.propertyTreesEqualIgnoreEmptyValues(config, otherConfig);
+            }
+
+            return api.ObjectHelper.equals(config, otherConfig);
+        }
+
+        static propertyTreesEqualIgnoreEmptyValues(config: PropertyTree, otherConfig: PropertyTree): boolean {
+            if ((!config || config.isEmpty()) && (!otherConfig || otherConfig.isEmpty())) {
                 return true;
             }
 
-            const data: PropertyTree = !!config && ignoreEmptyValues ? PropertyTreeHelper.trimPropertyTree(config) : config;
-            const otherData: PropertyTree = !!otherConfig && ignoreEmptyValues
-                                            ? PropertyTreeHelper.trimPropertyTree(otherConfig)
-                                            : otherConfig;
+            const data: PropertyTree = !!config ? PropertyTreeHelper.trimPropertyTree(config) : config;
+            const otherData: PropertyTree = !!otherConfig ? PropertyTreeHelper.trimPropertyTree(otherConfig) : otherConfig;
 
             return api.ObjectHelper.equals(data, otherData);
         }
