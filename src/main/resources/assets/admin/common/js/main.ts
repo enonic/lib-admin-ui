6358de5d7166cb834595ac5de2ca0aef57fdc9a7
Mikita Taukachou <edloidas@gmail.com>
2019-09-12 15:50:45 +0300

    Migrate to Webpack build #708
    Installed NPM packages.

--- src/main/resources/assets/admin/common/js/main.ts
+++ src/main/resources/assets/admin/common/js/main.ts
@@ -7,3 +7,7 @@ import {StyleHelper} from './StyleHelper';
  Prefix must match @_CLS_PREFIX in web\admin\common\styles\_module.less
  */
 StyleHelper.setCurrentPrefix(StyleHelper.ADMIN_PREFIX);
+
+declare var require: { context: (directory: string, useSubdirectories: boolean, filter: RegExp) => void };
+const importAll = r => r.keys().forEach(r);
+importAll(require.context('./', true, /^(?!\.[\/\\]types).*/));
