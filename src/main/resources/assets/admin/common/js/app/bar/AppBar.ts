115567253a79e187a1d2e2fbd40eb5eb5818dc36
Mikita Taukachou <edloidas@gmail.com>
2020-06-16 15:55:42 +0300

    Update AppIcon class to be customizable #1341

--- src/main/resources/assets/admin/common/js/app/bar/AppBar.ts
+++ src/main/resources/assets/admin/common/js/app/bar/AppBar.ts
@@ -2,9 +2,8 @@ import {DivEl} from '../../dom/DivEl';
 import {ActionContainer} from '../../ui/ActionContainer';
 import {ResponsiveManager} from '../../ui/responsive/ResponsiveManager';
 import {Action} from '../../ui/Action';
-import {ImgEl} from '../../dom/ImgEl';
-import {SpanEl} from '../../dom/SpanEl';
 import {ShowAppLauncherAction} from './ShowAppLauncherAction';
+import {AppIcon} from './AppIcon';
 import {AppBarActions} from './AppBarActions';
 import {Application} from '../Application';
 
@@ -49,41 +48,4 @@ export class AppBar
     setHomeIconAction() {
         this.appIcon.setAction(AppBarActions.SHOW_BROWSE_PANEL);
     }
-}
-
-export class AppIcon
-    extends DivEl {
-
-    private readonly appNameSpan: SpanEl;
-
-    constructor(app: Application, action?: Action) {
-
-        super('home-button');
-
-            if (app.getIconUrl()) {
-                const icon = new ImgEl(app.getIconUrl(), 'app-icon');
-                if (app.getIconTooltip()) {
-                    icon.getEl().setTitle(app.getIconTooltip());
-                }
-                this.appendChild(icon);
-            }
-
-            this.appNameSpan = new SpanEl('app-name');
-            this.appNameSpan.setHtml(app.getName());
-            this.appendChild(this.appNameSpan);
-
-        if (action) {
-            this.setAction(action);
-        }
-    }
-
-    setAppName(name: string) {
-        this.appNameSpan.setHtml(name);
-    }
-
-    setAction(action: Action) {
-        this.addClass('clickable');
-        this.onClicked(() => action.execute());
-    }
-
 }
