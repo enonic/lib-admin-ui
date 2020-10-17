e94af4f5e3a7740a69a3855af97d4537b1043c5b
Alan Semenov <ase@enonic.com>
2020-10-17 12:05:48 +0200

    Support POST in GetMessagesRequest (#1502)

--- src/main/resources/assets/admin/common/js/util/MessagesInitializer.ts
+++ src/main/resources/assets/admin/common/js/util/MessagesInitializer.ts
@@ -3,14 +3,18 @@ import {JsonResponse} from '../rest/JsonResponse';
 import {Path} from '../rest/Path';
 import {Messages} from './Messages';
 import {ResourceRequest} from '../rest/ResourceRequest';
+import {HttpMethod} from '../rest/HttpMethod';
 
-export function i18nInit(url: string): Q.Promise<void> {
-
+export function i18nInit(url: string, bundles?: string[]): Q.Promise<void> {
     if (!Messages.isEmpty()) {
         return Q(null);
     }
 
-    const request: GetMessagesRequest = new GetMessagesRequest(url);
+    const request: GetMessagesRequest = new GetMessagesRequest(url, bundles);
+    if (!!bundles && bundles.length) {
+        request.setMethod(HttpMethod.POST);
+        request.setIsFormRequest(true);
+    }
 
     return request.send().then((response: JsonResponse<KeysJson>) => {
         const messages: KeysJson = response.getResult();
@@ -25,16 +29,28 @@ export interface KeysJson {
 class GetMessagesRequest
     extends ResourceRequest<void> {
 
-    private url: string;
+    private readonly url: string;
 
-    constructor(url: string) {
+    private readonly bundles: string[];
+
+    constructor(url: string, bundles?: string[]) {
         super();
 
         this.url = url;
+        this.bundles = bundles;
     }
 
     getRequestPath(): Path {
         return Path.fromString(this.url);
+    }
+
+    getParams(): Object {
+        if (!this.bundles) {
+            return {};
+        }
+        return {
+            bundles: this.bundles
+        }
     }
 
 }
