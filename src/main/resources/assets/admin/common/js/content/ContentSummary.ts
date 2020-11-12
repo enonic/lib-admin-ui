c5a24b25306c431eb87c5296db455ebb542f3d12
Alan Semenov <ase@enonic.com>
2020-11-12 08:43:09 +0100

    Codacy

--- src/main/resources/assets/admin/common/js/content/ContentSummary.ts
+++ src/main/resources/assets/admin/common/js/content/ContentSummary.ts
@@ -479,7 +479,8 @@ export class ContentSummaryBuilder {
 
         this.contentState = ContentState.fromString(json.contentState);
         this.workflow = Workflow.fromJson(json.workflow);
-        this.inherit = json.inherit && json.inherit.length > 0 ? json.inherit.map(type => ContentInheritType[type]) : [];
+        this.inherit = json.inherit &&
+                       json.inherit.length > 0 ? json.inherit.map((inheritType: string) => ContentInheritType[inheritType]) : [];
 
         return this;
     }
