fc90986c306831c2789478708282dc93f2078883
Alex Shklianko <asl@enonic.com>
2020-12-14 16:49:49 +0300

    TreeGrid: trigger change event on node insert (#1640)
    * TreeGrid: trigger change event on node insert #1639
    
    * TreeGrid: trigger change event on node insert #1639
    
    -Not triggering add event on init data

--- src/main/resources/assets/admin/common/js/ui/treegrid/TreeGrid.ts
+++ src/main/resources/assets/admin/common/js/ui/treegrid/TreeGrid.ts
@@ -686,7 +686,6 @@ export class TreeGrid<DATA extends IDentifiable>
     initData(nodes: TreeNode<DATA>[]) {
         this.gridData.setItems(nodes, this.idPropertyName);
         this.resetCurrentSelection(nodes);
-        this.notifyDataChanged(new DataChangedEvent<DATA>(nodes, DataChangedType.ADDED));
     }
 
     expandNodeByDataId(dataId: string): Q.Promise<boolean> {
@@ -1752,6 +1751,7 @@ export class TreeGrid<DATA extends IDentifiable>
         parent.insertChild(nodeToInsert, index);
         parent.setExpandable(true);
         this.invalidateNodes([parent]);
+        this.notifyDataChanged(new DataChangedEvent<DATA>([nodeToInsert], DataChangedType.ADDED));
     }
 
     private getIndexRelativeToParent(parent: TreeNode<DATA>, index: number): number {
