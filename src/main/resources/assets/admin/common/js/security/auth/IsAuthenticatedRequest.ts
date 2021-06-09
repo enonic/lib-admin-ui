f2a1c2b5eced9fbdf4756c22ed128b152f6f8070
Aliaksandr Shklianko <asl@enonic.com>
2021-06-09 11:00:40 +0300

    IsAuthenticatedRequest: cache LoginResult? #1648
    -Using cache in auth request to prevent extra requests. Can be removed any time when auth meta will be propagated into live session

--- src/main/resources/assets/admin/common/js/security/auth/IsAuthenticatedRequest.ts
+++ src/main/resources/assets/admin/common/js/security/auth/IsAuthenticatedRequest.ts
@@ -1,3 +1,4 @@
+import * as Q from 'q';
 import {JsonResponse} from '../../rest/JsonResponse';
 import {AuthResourceRequest} from './AuthResourceRequest';
 import {LoginResult} from './LoginResult';
@@ -6,13 +7,21 @@ import {LoginResultJson} from './LoginResultJson';
 export class IsAuthenticatedRequest
     extends AuthResourceRequest<LoginResult> {
 
+    private static CACHE: LoginResult;
+
     constructor() {
         super();
         this.addRequestPathElements('authenticated');
     }
 
     protected parseResponse(response: JsonResponse<LoginResultJson>): LoginResult {
-        return new LoginResult(response.getResult());
+        IsAuthenticatedRequest.CACHE = new LoginResult(response.getResult());
+
+        return IsAuthenticatedRequest.CACHE;
+    }
+
+    sendAndParse(): Q.Promise<LoginResult> {
+        return !!IsAuthenticatedRequest.CACHE ? Q(IsAuthenticatedRequest.CACHE) : super.sendAndParse();
     }
 
 }
