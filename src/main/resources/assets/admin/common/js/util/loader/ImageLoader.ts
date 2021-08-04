a85e934f2450463490d2b2aa1f3b8a5360cea3d6
Aliaksandr Shklianko <asl@enonic.com>
2021-08-04 17:26:51 +0300

    Application Item Statistics Panel - panel is not refreshed after changing a selection in the grid. Incorrect state is displayed in the panel #2077
    -Totally broken code fixed

--- src/main/resources/assets/admin/common/js/util/loader/ImageLoader.ts
+++ src/main/resources/assets/admin/common/js/util/loader/ImageLoader.ts
@@ -1,28 +1,20 @@
 export class ImageLoader {
 
-    private static cachedImages: HTMLImageElement[] = [];
+    private static cachedImages: Map<string,HTMLImageElement> = new Map();
 
     static get(url: string, width?: number, height?: number): HTMLImageElement {
-        let imageFound: boolean = false;
-        let returnImage: HTMLImageElement;
-        url = encodeURI(url);
+        const encodedUrl: string = encodeURI(url);
 
-        for (let cachedImage of ImageLoader.cachedImages) {
-            if (cachedImage.src === url) {
-                imageFound = true;
-                returnImage = cachedImage;
-            }
-        }
+        return ImageLoader.cachedImages.has(encodedUrl) ? ImageLoader.cachedImages.get(encodedUrl) :
+               this.createImage(encodedUrl, width, height);
+    }
 
-        if (!imageFound) {
-            let image: HTMLImageElement = new Image(width, height);
-            image.src = url;
-            //image.height = height;
-            //image.width = width;
-            ImageLoader.cachedImages[ImageLoader.cachedImages.length + 1] = image;
-            returnImage = image;
-        }
+    private static createImage(url: string, width?: number, height?: number): HTMLImageElement {
+        const image: HTMLImageElement = new Image(width, height);
+        image.src = url;
 
-        return returnImage;
+        ImageLoader.cachedImages.set(url, image);
+
+        return image;
     }
 }
