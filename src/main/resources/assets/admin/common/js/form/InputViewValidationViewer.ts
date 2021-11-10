1911f0f02af31d89022dc0cd6b5f9d875c4871b9
Alan Semenov <ase@enonic.com>
2021-11-10 14:51:23 +0100

    Implemented link in validation message to execute specified callback (#2258)

--- src/main/resources/assets/admin/common/js/form/InputViewValidationViewer.ts
+++ src/main/resources/assets/admin/common/js/form/InputViewValidationViewer.ts
@@ -1,8 +1,12 @@
 import {Viewer} from '../ui/Viewer';
 import {InputValidationRecording} from './inputtype/InputValidationRecording';
 import {i18n} from '../util/Messages';
+import {AEl} from '../dom/AEl';
 
 export class InputViewValidationViewer extends Viewer<InputValidationRecording> {
+
+    private static ERROR_DETAILS_VISIBLE_CLS: string = 'error-details-visible';
+    private static ERROR_DETAILS_TOGGLER_CLS: string = 'error-details-toggler';
 
     constructor() {
         super('validation-viewer');
@@ -13,7 +17,34 @@ export class InputViewValidationViewer extends Viewer<InputValidationRecording> 
 
         if (recording) {
             this.setHtml(recording.isValidationMessageToBeRendered() ? this.getText() : '');
+            if (recording.isValidationMessageToBeRendered() && recording.hasToggleErrorDetailsCallback()) {
+                this.appendErrorDetailsToggler(recording, this.hasClass(InputViewValidationViewer.ERROR_DETAILS_VISIBLE_CLS));
+            }
         }
+    }
+
+    private appendErrorDetailsToggler(recording: InputValidationRecording, isErrorDetailsVisible: boolean) {
+        const toggleLink = new AEl(InputViewValidationViewer.ERROR_DETAILS_TOGGLER_CLS);
+
+        toggleLink.whenRendered(() =>
+            toggleLink.setHtml(
+        isErrorDetailsVisible ? i18n('field.validation.hideDetails') : i18n('field.validation.showDetails')
+            )
+        );
+
+        toggleLink.onClicked((e: MouseEvent) => {
+            const errorDetailsVisible = this.hasClass(InputViewValidationViewer.ERROR_DETAILS_VISIBLE_CLS);
+            this.toggleClass(InputViewValidationViewer.ERROR_DETAILS_VISIBLE_CLS, !errorDetailsVisible);
+            toggleLink.setHtml(errorDetailsVisible ?
+                i18n('field.validation.showDetails') : i18n('field.validation.hideDetails')
+            );
+            recording.getToggleErrorDetailsCallback()();
+
+            e.stopPropagation();
+            e.preventDefault();
+        });
+
+        this.appendChild(toggleLink);
     }
 
     private getText(): string {
