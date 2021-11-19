2696cf697fad72837afd0149e03436f9cc835fd6
Aliaksandr Shklianko <asl@enonic.com>
2021-11-19 18:51:13 +0300

    Input with a mix of valid and invalid occurrences is considered valid #2275
    -Setting error message for custom errors in occurrences

--- src/main/resources/assets/admin/common/js/form/inputtype/support/BaseInputTypeNotManagingAdd.ts
+++ src/main/resources/assets/admin/common/js/form/inputtype/support/BaseInputTypeNotManagingAdd.ts
@@ -22,6 +22,7 @@ import {OccurrenceValidationRecord} from './OccurrenceValidationRecord';
 import {BaseInputType} from './BaseInputType';
 import {ValidationError} from '../../../ValidationError';
 import {AdditionalValidationRecord} from '../../AdditionalValidationRecord';
+import {i18n} from '../../../util/Messages';
 
 export abstract class BaseInputTypeNotManagingAdd
     extends BaseInputType {
@@ -256,13 +257,33 @@ export abstract class BaseInputTypeNotManagingAdd
 
     private updateValidationRecordAndNotifyIfChanged() {
         const totalValid: number = this.getTotalValidOccurrences();
+        const hasCustomError: boolean = this.hasCustomError();
         const newValidationRecord: InputValidationRecording = new InputValidationRecording(this.input.getOccurrences(), totalValid);
+
+        if (hasCustomError) {
+            newValidationRecord.setErrorMessage(i18n('field.custom.error'));
+        }
 
         if (newValidationRecord.validityChanged(this.previousValidationRecording)) {
             this.notifyValidityChanged(new InputValidityChangedEvent(newValidationRecord));
         }
 
         this.previousValidationRecording = newValidationRecord;
+    }
+
+    private hasCustomError(): boolean {
+        let hasCustomError: boolean = false;
+
+        this.occurrenceValidationState.forEach((occurrenceValidationRecord: OccurrenceValidationRecord) => {
+            const isCustomError: boolean =
+                occurrenceValidationRecord.getAdditionalValidationRecords().some((rec: AdditionalValidationRecord) => rec.isCustom());
+
+            if (isCustomError) {
+                hasCustomError = true;
+            }
+        });
+
+        return hasCustomError;
     }
 
     private updateValidationRecord() {
@@ -352,7 +373,7 @@ export abstract class BaseInputTypeNotManagingAdd
         this.getContext().formContext.getValidationErrors().forEach((error: ValidationError) => {
             if (occurrenceDataPath === error.getPropertyPath()) {
                 this.occurrenceValidationState.get(occurrenceId).addAdditionalValidation(
-                    AdditionalValidationRecord.create().setMessage(error.getMessage()).build());
+                    AdditionalValidationRecord.create().setMessage(error.getMessage()).setCustom(true).build());
             }
         });
     }
