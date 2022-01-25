e642360c03e2700a79c87d1a603c0509f9a04895
Pavel Milkevich <pavel.milkevich@gmail.com>
2022-01-25 16:30:32 +0300

    Create head request #2389
    (cherry picked from commit 450bb3429c8412e51e6ccfcbe26cbe4803c7ed31)

---
+++ src/main/resources/assets/admin/common/js/rest/HeadRequest.ts
@@ -0,0 +1,27 @@
+import {Request} from './Request';
+import {BrowserHelper} from '../BrowserHelper';
+import {HttpMethod} from './HttpMethod';
+import {UriHelper} from '../util/UriHelper';
+
+export class HeadRequest
+    extends Request {
+
+    constructor() {
+        super(HttpMethod.HEAD);
+    }
+
+    protected prepareRequest(): void {
+        super.prepareRequest();
+
+        if (BrowserHelper.isIE()) {
+            this.request.setRequestHeader('Pragma', 'no-cache');
+            this.request.setRequestHeader('Cache-Control', 'no-cache');
+        }
+    }
+
+    protected createRequestURI(): string {
+        const uriString: string = UriHelper.appendUrlParams(this.path.toString(), this.params);
+        return UriHelper.getUri(uriString);
+    }
+
+}
