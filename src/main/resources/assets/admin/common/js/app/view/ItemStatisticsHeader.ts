6ec02c500c64e198585c20f3fe48d1bf7b9755da
ashklianko <asl@enonic.com>
2022-10-26 16:47:07 +0200

    Refactor ItemStatisticsHeader (#2978)

--- src/main/resources/assets/admin/common/js/app/view/ItemStatisticsHeader.ts
+++ src/main/resources/assets/admin/common/js/app/view/ItemStatisticsHeader.ts
@@ -7,90 +7,95 @@ import {ImgEl} from '../../dom/ImgEl';
 import {ImgHelper} from '../../dom/ImgElHelper';
 import {SpanEl} from '../../dom/SpanEl';
 import {ViewItem} from './ViewItem';
+import * as Q from 'q';
 
 export class ItemStatisticsHeader
     extends DivEl {
 
-    private browseItem: ViewItem;
+    protected browseItem: ViewItem;
 
-    private iconEl: Element;
+    protected iconEl: Element;
 
-    private readonly headerTitleEl: H1El;
+    protected readonly headerTitleEl: H1El;
 
-    private readonly headerPathEl: H4El;
+    protected readonly headerPathEl: H4El;
+
+    protected readonly iconBlock: Element;
 
     constructor() {
         super('header');
+
+        this.iconBlock = new DivEl('icon-block');
         this.headerTitleEl = new H1El('title');
         this.headerPathEl = new H4El('path');
-        this.appendChild(this.headerTitleEl);
-        this.appendChild(this.headerPathEl);
     }
 
-    setItem(item: ViewItem) {
-        if (this.iconEl) {
-            this.iconEl.remove();
-        }
-
-        if (item) {
-            this.iconEl = this.createIconEl(item);
-            this.prependChild(this.iconEl);
-
-            let displayName = item.getDisplayName() || '';
-            this.headerTitleEl.setHtml(displayName);
-            this.headerTitleEl.getEl().setAttribute('title', displayName);
-
-            this.headerPathEl.removeChildren();
-        }
-
+    setItem(item: ViewItem): void {
         this.browseItem = item;
-    }
+        this.iconEl?.remove();
 
-    setIconUrl(value: string) {
-        if (this.iconEl) {
-            this.iconEl.remove();
+        if (!item) {
+            return;
         }
 
-        let size = this.getIconSize(this.browseItem);
-        let icon: HTMLImageElement = ImageLoader.get(value, size, size);
+        this.iconEl = this.createIconEl().addClass('font-icon-default');
+        this.iconBlock.appendChild(this.iconEl);
 
-        this.iconEl = <ImgEl> new Element(new NewElementBuilder().setTagName('img').setHelper(
-            new ImgHelper(icon)));
+        const displayName: string = item.getDisplayName() || '';
+        this.headerTitleEl.setHtml(displayName);
+        this.headerTitleEl.getEl().setAttribute('title', displayName);
 
-        this.prependChild(this.iconEl);
+        this.headerPathEl.removeChildren();
     }
 
-    setHeaderSubtitle(value: string, className: string) {
+    setIconUrl(value: string): void {
+        this.iconEl?.remove();
+        this.iconEl = this.doCreateIconEl(value);
+        this.iconBlock.appendChild(this.iconEl);
+    }
+
+    private doCreateIconEl(src: string): Element {
+        const size: number = this.getIconSize();
+        const icon: HTMLImageElement = ImageLoader.get(src, size, size);
+        return new Element(new NewElementBuilder().setTagName('img').setHelper(new ImgHelper(icon)));
+    }
+
+    setHeaderSubtitle(value: string, className: string): void {
         this.headerPathEl.removeChildren();
         this.appendToHeaderPath(value, className);
     }
 
-    private createIconEl(item: ViewItem) {
-        let iconEl: Element;
-
-        if (typeof item.getIconSrc === 'function' && item.getIconSrc()) {
-            iconEl = new ImgEl(item.getIconSrc());
-        } else if (item.getIconUrl()) {
-            let size = this.getIconSize(item);
-            let icon: HTMLImageElement = ImageLoader.get(item.getIconUrl(), size, size);
-
-            iconEl = <ImgEl> new Element(new NewElementBuilder().setTagName('img').setHelper(new ImgHelper(icon)));
-        } else {
-            iconEl = new DivEl(item.getIconClass());
+    private createIconEl(): Element {
+        if (typeof this.browseItem.getIconSrc === 'function' && this.browseItem.getIconSrc()) {
+            return new ImgEl(this.browseItem.getIconSrc());
         }
 
-        iconEl.addClass('font-icon-default');
+        if (this.browseItem.getIconUrl()) {
+            return this.doCreateIconEl(this.browseItem.getIconUrl());
+        }
 
-        return iconEl;
+        return new DivEl(this.browseItem.getIconClass());
     }
 
-    protected appendToHeaderPath(value: string, className: string) {
+    protected appendToHeaderPath(value: string, className: string): void {
         const pathName: SpanEl = new SpanEl(className);
         pathName.getEl().setInnerHtml(value);
         this.headerPathEl.appendChild(pathName);
     }
 
-    protected getIconSize(item: ViewItem): number {
+    protected getIconSize(): number {
         return 64;
+    }
+
+    doRender(): Q.Promise<boolean> {
+        return super.doRender().then(rendered => {
+            this.appendChild(this.iconBlock);
+
+            const titleAndPathBlock: DivEl = new DivEl('names-block');
+            titleAndPathBlock.appendChildren(this.headerTitleEl, this.headerPathEl);
+            this.appendChild(titleAndPathBlock);
+
+            return rendered;
+        });
     }
 }
