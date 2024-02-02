aa66b710375d7c6da11038720bce14a5f29b1c02
ashklianko <asl@enonic.com>
2024-02-02 15:55:01 +0100

    PrincipalComboBox: refactor to remove usage of a slickgrid #3392

---
+++ src/main/resources/assets/admin/common/js/ui/security/PrincipalsListBox.ts
@@ -0,0 +1,34 @@
+import {LazyListBox} from '../selector/list/LazyListBox';
+import {Principal} from '../../security/Principal';
+import {PrincipalViewer} from './PrincipalViewer';
+import {PrincipalLoader} from '../../security/PrincipalLoader';
+
+export class PrincipalsListBox
+    extends LazyListBox<Principal> {
+
+    private readonly loader: PrincipalLoader;
+
+    constructor(loader: PrincipalLoader) {
+        super('principals-list-box');
+
+        this.loader = loader;
+    }
+
+    protected createItemView(item: Principal, readOnly: boolean): PrincipalViewer {
+        const viewer = new PrincipalViewer();
+        viewer.setObject(item);
+        return viewer;
+    }
+
+    protected getItemId(item: Principal): string {
+        return item.getKey().toString();
+    }
+
+    protected handleLazyLoad(): void {
+        super.handleLazyLoad();
+
+        if (this.loader.isPartiallyLoaded()) {
+            this.loader.load(true);
+        }
+    }
+}
