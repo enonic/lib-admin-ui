4f6a5fba442c3f06af82bd2424a9aecfdba869a4
ashklianko <asl@enonic.com>
2024-02-12 22:20:03 +0100

    Slickgrid: small ref

--- src/main/resources/assets/admin/common/js/ui/selector/list/FilterableListBoxWrapperWithSelectedView.ts
+++ src/main/resources/assets/admin/common/js/ui/selector/list/FilterableListBoxWrapperWithSelectedView.ts
@@ -13,13 +13,9 @@ export interface ListBoxInputOptions<I> extends FilterableListBoxOptions<I> {
 export abstract class FilterableListBoxWrapperWithSelectedView<I>
     extends FilterableListBoxWrapper<I> {
 
-    public static LIMIT_REACHED_CLASS: string = 'selection-limit-reached';
-
     protected selectedOptionsView: BaseSelectedOptionsView<I>;
 
     protected options: ListBoxInputOptions<I>;
-
-    protected selectionLimitReached: boolean = false;
 
     protected constructor(listBox: ListBox<I>, options?: ListBoxInputOptions<I>) {
         super(listBox, options);
@@ -48,8 +44,6 @@ export abstract class FilterableListBoxWrapperWithSelectedView<I>
         if (!this.selectedOptionsView.isSelected(optionToSelect)) {
             this.selectedOptionsView.addOption(optionToSelect, true, -1);
         }
-
-        this.checkSelectionLimitReached();
     }
 
     protected doDeselect(itemToDeselect: I): void {
@@ -61,32 +55,9 @@ export abstract class FilterableListBoxWrapperWithSelectedView<I>
         if (existing) {
             this.selectedOptionsView.removeOption(this.createSelectedOption(itemToDeselect), true);
         }
-        this.checkSelectionLimitReached();
     }
 
     abstract createSelectedOption(item: I): Option<I>;
-
-    protected checkSelectionLimitReached(): void {
-        const isMaxOccurrencesReached = this.maximumOccurrencesReached();
-
-        if (this.selectionLimitReached && !isMaxOccurrencesReached) {
-            this.selectionLimitReached = false;
-            this.handleSelectionLimitIsNoLongerReached();
-        } else if (!this.selectionLimitReached && isMaxOccurrencesReached) {
-            this.selectionLimitReached = true;
-            this.handleSelectionLimitReached();
-        }
-    }
-
-    protected handleSelectionLimitReached(): void {
-        this.filterAndListContainer.hide();
-        this.toggleClass(FilterableListBoxWrapperWithSelectedView.LIMIT_REACHED_CLASS, true);
-    }
-
-    protected handleSelectionLimitIsNoLongerReached(): void {
-        this.filterAndListContainer.show();
-        this.toggleClass(FilterableListBoxWrapperWithSelectedView.LIMIT_REACHED_CLASS, false);
-    }
 
     doRender(): Q.Promise<boolean> {
         return super.doRender().then((rendered: boolean) => {
@@ -99,10 +70,6 @@ export abstract class FilterableListBoxWrapperWithSelectedView<I>
 
     countSelected(): number {
         return this.selectedOptionsView.count();
-    }
-
-    maximumOccurrencesReached(): boolean {
-        return this.selectedOptionsView.maximumOccurrencesReached();
     }
 
     getSelectedOptions(): SelectedOption<I>[] {
