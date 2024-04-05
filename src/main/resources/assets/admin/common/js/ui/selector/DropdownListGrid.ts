14f72af15dbbc6aae86d5b23c1ca77f83acfe305
ashklianko <asl@enonic.com>
2024-04-05 18:49:41 +0200

    Non-selectable options in dropdowns should not be interactable #3521

--- src/main/resources/assets/admin/common/js/ui/selector/DropdownListGrid.ts
+++ src/main/resources/assets/admin/common/js/ui/selector/DropdownListGrid.ts
@@ -3,6 +3,8 @@ import {Grid} from '../grid/Grid';
 import {DataView} from '../grid/DataView';
 import {DropdownGrid, DropdownGridConfig} from './DropdownGrid';
 import {Option} from './Option';
+import {i18n} from '../../util/Messages';
+import {StringHelper} from '../../util/StringHelper';
 
 export class DropdownListGrid<OPTION_DISPLAY_VALUE>
     extends DropdownGrid<OPTION_DISPLAY_VALUE> {
@@ -31,10 +33,37 @@ export class DropdownListGrid<OPTION_DISPLAY_VALUE>
 
         this.grid = new Grid<Option<OPTION_DISPLAY_VALUE>>(this.gridData, this.createColumns(), this.createOptions());
 
+        this.gridData.setItemMetadataHandler(this.handleItemMetadata.bind(this));
+
         this.gridData.onRowCountChanged(() => this.notifyRowCountChanged());
     }
 
     protected getGridData(): DataView<Option<OPTION_DISPLAY_VALUE>> {
         return this.gridData;
+    }
+
+    protected handleItemMetadata(row: number) {
+        let option = this.gridData.getItem(row);
+        let cssClasses = '';
+        let title = '';
+
+        if (option.isReadOnly()) {
+            cssClasses += ' readonly';
+            title = i18n('field.readOnly');
+        }
+
+        if (option.isSelectable()) {
+            cssClasses += ' selectable';
+        }
+
+        if (option.isExpandable()) {
+            cssClasses += ' expandable';
+        }
+
+        if (!StringHelper.isBlank(cssClasses) || !StringHelper.isBlank(title)) {
+            return {cssClasses: cssClasses, title: title};
+        }
+
+        return null;
     }
 }
