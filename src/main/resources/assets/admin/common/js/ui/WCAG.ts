671ef4d96bc8eb317dc7ac19cf83e22e323c7901
alansemenov <ase@enonic.com>
2024-07-05 16:27:11 +0200

    Implement an interface with WCAG properties #3641

--- src/main/resources/assets/admin/common/js/ui/WCAG.ts
+++ src/main/resources/assets/admin/common/js/ui/WCAG.ts
@@ -1,3 +1,15 @@
-export interface WCAG {
-    applyWCAGAttributes(): void;
+export const IWCAG = Symbol('IWCAG');
+
+export interface IWCAG {
+    [IWCAG]: boolean;
+    ariaLabel?: string;
+    role?: AriaRole;
+    tabbable?: boolean;
+}
+
+export enum AriaRole {
+    NONE = 'presentation',
+    BANNER = 'banner',
+    BUTTON = 'button',
+    TOOLBAR = 'toolbar'
 }
