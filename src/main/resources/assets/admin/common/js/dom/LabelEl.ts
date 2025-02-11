4e768e5c31aeca61ebf804481da1c7097c1ac1fb
ashklianko <asl@enonic.com>
2025-02-11 08:23:24 +0100

    Fix label text overflow #3878

--- src/main/resources/assets/admin/common/js/dom/LabelEl.ts
+++ src/main/resources/assets/admin/common/js/dom/LabelEl.ts
@@ -1,10 +1,16 @@
 import {Element, NewElementBuilder} from './Element';
+import {SpanEl} from './SpanEl';
 
 export class LabelEl
     extends Element {
 
+    private readonly textContainer: Element;
+
     constructor(value: string, forElement?: Element, className?: string) {
         super(new NewElementBuilder().setTagName('label').setClassName(className));
+
+        this.textContainer = new SpanEl('label-text');
+        this.appendChild(this.textContainer);
         this.setValue(value);
         if (forElement) {
             this.setForElement(forElement);
@@ -16,10 +22,10 @@ export class LabelEl
     }
 
     setValue(value: string) {
-        this.getEl().setInnerHtml(value);
+        this.textContainer.getEl().setInnerHtml(value);
     }
 
     getValue(): string {
-        return this.getEl().getInnerHtml();
+        return this.textContainer.getEl().getInnerHtml();
     }
 }
