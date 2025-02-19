c220f7522aafa6bd1e3e0b836734a602a6e20173
alansemenov <ase@enonic.com>
2025-02-19 14:47:15 +0100

    Don't create empty LabelEl

--- src/main/resources/assets/admin/common/js/dom/LabelEl.ts
+++ src/main/resources/assets/admin/common/js/dom/LabelEl.ts
@@ -1,20 +1,31 @@
+import {StringHelper} from '../util/StringHelper';
 import {Element, NewElementBuilder} from './Element';
 import {SpanEl} from './SpanEl';
 
 export class LabelEl
     extends Element {
 
-    private readonly textContainer: Element;
+    private textContainer: Element;
 
     constructor(value: string, forElement?: Element, className?: string) {
         super(new NewElementBuilder().setTagName('label').setClassName(className));
 
-        this.textContainer = new SpanEl('label-text');
-        this.appendChild(this.textContainer);
-        this.setValue(value);
+        if (!StringHelper.isBlank(value)) {
+            this.setValue(value);
+        }
+
         if (forElement) {
             this.setForElement(forElement);
         }
+    }
+
+    private appendTextContainer() {
+        if (this.textContainer) {
+            return;
+        }
+
+        this.textContainer = new SpanEl('label-text');
+        this.appendChild(this.textContainer);
     }
 
     setForElement(forElement: Element) {
@@ -22,10 +33,18 @@ export class LabelEl
     }
 
     setValue(value: string) {
+        if (!this.textContainer) {
+            this.appendTextContainer();
+        }
+
         this.textContainer.getEl().setInnerHtml(value);
     }
 
     getValue(): string {
+        if (!this.textContainer) {
+            return '';
+        }
+
         return this.textContainer.getEl().getInnerHtml();
     }
 }
