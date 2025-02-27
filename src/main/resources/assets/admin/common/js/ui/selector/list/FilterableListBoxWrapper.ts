e76ad5d66016fdb174b0ec1d369d7ee619735629
ashklianko <asl@enonic.com>
2025-02-27 10:04:49 +0100

    Dropdowns: remove rendered elements from DOM on hide #3905

--- src/main/resources/assets/admin/common/js/ui/selector/list/FilterableListBoxWrapper.ts
+++ src/main/resources/assets/admin/common/js/ui/selector/list/FilterableListBoxWrapper.ts
@@ -128,12 +128,20 @@ export class FilterableListBoxWrapper<I>
     }
 
     protected doShowDropdown(): void {
+        if (!this.hasChild(this.listBox)) {
+            this.attachListBoxOnShown();
+        }
+
         this.listBox.show();
 
         if (this.loadWhenListShown) {
             this.loadListOnShown();
             this.loadWhenListShown = false;
         }
+    }
+
+    protected attachListBoxOnShown(): void {
+        this.appendChild(this.listBox);
     }
 
     protected loadListOnShown(): void {
@@ -150,6 +158,7 @@ export class FilterableListBoxWrapper<I>
 
     protected doHideDropdown(): void {
         this.listBox.hide();
+        this.listBox.remove();
     }
 
     protected listenClickOutside(): void {
@@ -329,7 +338,7 @@ export class FilterableListBoxWrapper<I>
         });
 
         this.selectionDelta = new Map();
-        this.listBox.hide();
+        this.doHideDropdown();
         this.dropdownHandle.up();
 
         Array.from(this.itemsWrappers.values()).forEach(
