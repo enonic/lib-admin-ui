b4728a7fb6e91a30c3937ef380745706a58b54fa
alansemenov <ase@enonic.com>
2025-10-20 09:35:44 +0200

    AccessDeniedException class should not be used #4176

--- src/main/resources/assets/admin/common/js/DefaultErrorHandler.ts
+++ src/main/resources/assets/admin/common/js/DefaultErrorHandler.ts
@@ -1,20 +1,12 @@
 import {ObjectHelper} from './ObjectHelper';
-import {Application} from './app/Application';
-import {ShowAppLauncherEvent} from './app/ShowAppLauncherEvent';
-import {AccessDeniedException} from './AccessDeniedException';
 import {Exception, ExceptionType} from './Exception';
 import {showError, showFeedback, showWarning} from './notify/MessageBus';
 
 export class DefaultErrorHandler {
 
     static handle(error: any): void {
-        if (ObjectHelper.iFrameSafeInstanceOf(error, AccessDeniedException)) {
-            let application: Application = Application.getApplication();
-            let wnd = application.getWindow();
-            new ShowAppLauncherEvent(application, true).fire(wnd.parent);
-            new ShowAppLauncherEvent(application, true).fire(wnd);
-        } else if (ObjectHelper.iFrameSafeInstanceOf(error, Exception)) {
-            let message = error.getMessage();
+        if (ObjectHelper.iFrameSafeInstanceOf(error, Exception)) {
+            const message = error.getMessage();
 
             switch (error.getType()) {
             case ExceptionType.ERROR:
