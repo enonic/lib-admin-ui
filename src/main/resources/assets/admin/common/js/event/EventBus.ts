cd0e88ec15469dbbd9bd109c54954e39e15741bf
Pavel Milkevich <pmi@enonic.com>
2025-11-08 17:33:03 +0100

    Changed most events in LiveEditPage.ts and LiveEditPageProxy.ts to extend IframeEvent, started updating InitializeLiveEditEvent.ts to serialize itself to message

--- src/main/resources/assets/admin/common/js/event/EventBus.ts
+++ src/main/resources/assets/admin/common/js/event/EventBus.ts
@@ -8,6 +8,7 @@ export class EventBus
 
     static get(contextWindow: Window = window): EventBus {
         if (!EventBus.instance) {
+            console.info(`Creating new EventBus instance by ${window.location.pathname} on ${contextWindow.location.pathname}`);
             EventBus.instance = new EventBus(contextWindow);
         } else if (EventBus.instance.contextWindow !== contextWindow) {
             throw new Error('EventBus instance already created with a different contextWindow');
@@ -15,7 +16,7 @@ export class EventBus
         return EventBus.instance;
     }
 
-    onEvent(eventName: string, handler: (event: Event) => void): HandlersMapEntry {
+    onEvent(eventName: string, handler: (event: Event) => void): HandlersMapEntry<Event> {
         const entry = super.onEvent(eventName, handler);
         entry.customHandler = (e: CustomEvent) => handler(e.detail);
         this.contextWindow.addEventListener(eventName, entry.customHandler);
@@ -24,7 +25,7 @@ export class EventBus
 
     unEvent(eventName: string, handler?: (event: Event) => void) {
         const entries = super.unEvent(eventName, handler);
-        entries.forEach((entry: HandlersMapEntry) => {
+        entries.forEach((entry: HandlersMapEntry<Event>) => {
             this.contextWindow.removeEventListener(eventName, entry.customHandler);
         });
         return entries;
--- src/main/resources/assets/admin/common/js/event/IframeEventBus.ts
+++ src/main/resources/assets/admin/common/js/event/IframeEventBus.ts
@@ -10,14 +10,25 @@ export class IframeEventBus
 
     private constructor(contextWindow: Window) {
         super(contextWindow);
-        this.ensureListener();
+
+        if (!this.isListening) {
+            this.contextWindow.addEventListener('message', this.handleMessageEvent);
+            this.isListening = true;
+        }
     }
 
-    static get(contextWindow: Window = window): IframeEventBus {
+    static init(contextWindow: Window): IframeEventBus {
+        if (IframeEventBus.instance) {
+            throw new Error(`IframeEventBus instance already initialized on ${IframeEventBus.instance.contextWindow.location.pathname}`);
+        }
+        console.info(`Initializing IframeEventBus instance by ${window.location.pathname} on ${contextWindow.location.pathname}`);
+        IframeEventBus.instance = new IframeEventBus(contextWindow);
+        return IframeEventBus.instance;
+    }
+
+    static get(): IframeEventBus {
         if (!IframeEventBus.instance) {
-            IframeEventBus.instance = new IframeEventBus(contextWindow);
-        } else if (IframeEventBus.instance.contextWindow !== contextWindow) {
-            throw new Error('IframeEventBus instance already created with a different contextWindow');
+            throw new Error('IframeEventBus instance should be initialized with a contextWindow first');
         }
         return IframeEventBus.instance;
     }
@@ -25,15 +36,8 @@ export class IframeEventBus
     public fireEvent(event: IframeEvent) {
         this.contextWindow.parent.postMessage({
             eventName: event.getName(),
-            detail: event
+            detail: event.toMessage()
         }, '*');
-    }
-
-    private ensureListener() {
-        if (!this.isListening) {
-            this.contextWindow.addEventListener('message', this.handleMessageEvent);
-            this.isListening = true;
-        }
     }
 
     private handleMessageEvent = (event: MessageEvent) => {
@@ -41,6 +45,7 @@ export class IframeEventBus
         if (!eventName) {
             return;
         }
+        //TODO: create event object from detail!
         (this.handlersMap[eventName] || []).forEach(entry => entry.handler(detail));
     };
 
