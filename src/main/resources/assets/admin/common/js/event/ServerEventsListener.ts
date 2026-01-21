7948b44ffac780d50582e969e2b7b0b1d427b860
Pavel Milkevich <pmi@enonic.com>
2026-01-21 10:07:43 +0100

    Introduce IframeEventBus for iframe - CS events #4183
    - revert ServerEventsListener.ts deletion

---
+++ src/main/resources/assets/admin/common/js/event/ServerEventsListener.ts
@@ -0,0 +1,77 @@
+import {ServerEventsConnection} from './ServerEventsConnection';
+import {Application} from '../app/Application';
+import {Event} from './Event';
+import {EventJson} from './EventJson';
+import {ServerEventsTranslator} from './ServerEventsTranslator';
+
+export class ServerEventsListener {
+
+    private serverEventsConnection: ServerEventsConnection;
+
+    private applications: Application[];
+
+    constructor(applications: Application[], eventApiUrl: string) {
+        this.applications = applications;
+        this.serverEventsConnection = ServerEventsConnection.get(eventApiUrl);
+        this.serverEventsConnection.onServerEvent((event: Event) => this.onServerEvent(event));
+        this.serverEventsConnection.onUnknownServerEvent((eventJson: EventJson) => this.onUnknownServerEvent(eventJson));
+    }
+
+    getApplications(): Application[] {
+        return this.applications;
+    }
+
+    start() {
+        this.serverEventsConnection.connect();
+    }
+
+    stop() {
+        this.serverEventsConnection.disconnect();
+    }
+
+    setServerEventsTranslator(serverEventsTranslator: ServerEventsTranslator) {
+        this.serverEventsConnection.setServerEventsTranslator(serverEventsTranslator);
+    }
+
+    onConnectionLost(listener: () => void) {
+        this.serverEventsConnection.onDisconnected(listener);
+    }
+
+    unConnectionLost(listener: () => void) {
+        this.serverEventsConnection.unDisconnected(listener);
+    }
+
+    onConnectionRestored(listener: () => void) {
+        this.serverEventsConnection.onConnected(listener);
+    }
+
+    unConnectionRestored(listener: () => void) {
+        this.serverEventsConnection.unConnected(listener);
+    }
+
+    onConnectionError(listener: () => void) {
+        this.serverEventsConnection.onConnectionError(listener);
+    }
+
+    unConnectionError(listener: () => void) {
+        this.serverEventsConnection.unConnectionError(listener);
+    }
+
+    protected onServerEvent(event: Event) {
+        this.fireEvent(event);
+    }
+
+    protected onUnknownServerEvent(_eventJson: EventJson) {
+        //
+    }
+
+    protected fireEvent(event: Event) {
+        this.applications.forEach((application) => {
+            let appWindow = application.getWindow();
+            if (appWindow) {
+                event.fire(appWindow);
+            }
+        });
+    }
+
+}
