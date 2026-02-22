837ed0fc445143f5136ff0ee22d685be73ab5bba
Mikita Taukachou <edloidas@gmail.com>
2026-02-23 00:18:04 +0300

    Update UnsupportedInput styles
    Added UnsupportedInput stories.
    Added more styles to make it compact and distinct.

--- src/main/resources/assets/admin/common/js/form/inputtype2/UnsupportedInput.tsx
+++ src/main/resources/assets/admin/common/js/form/inputtype2/UnsupportedInput.tsx
@@ -1,6 +1,12 @@
+import {useI18n} from './I18nContext';
 import type {InputTypeComponentProps} from './types';
 
 export function UnsupportedInput({input}: InputTypeComponentProps) {
+    const t = useI18n();
     const typeName = input.getInputType().getName();
-    return <div class='unsupported-input-type'>Unsupported input type: {typeName}</div>;
+    return (
+        <div class='flex min-h-12 cursor-default items-center justify-center rounded border border-bdr-subtle border-dashed px-4.5 py-3 text-subtle text-xs'>
+            {t('field.value.unsupportedType', typeName)}
+        </div>
+    );
 }
