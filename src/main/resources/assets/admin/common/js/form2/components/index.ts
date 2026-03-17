54e904ef3b1c9b2491d80eef0a12b2d6d8f39048
Bruno Reis <reisfmb@gmail.com>
2026-03-17 19:09:13 -0300

    SortableList #4348

--- src/main/resources/assets/admin/common/js/form2/components/index.ts
+++ src/main/resources/assets/admin/common/js/form2/components/index.ts
@@ -9,6 +9,7 @@ export * from './instant-input';
 export * from './long-input';
 export * from './occurrence-list';
 export * from './radio-button-input';
+export * from './sortable-list';
 export * from './tag-input';
 export * from './text-area-input';
 export * from './text-line-input';
